581b0c6f2da5edadbfa92a25348db30044b757f1
andy.saunders <andy.saunders@e2x.co.uk>
2012-11-05 12:36:00 +0000

    Tidied up the compile.js file, error messaging etc. Recompiled the linux binding.node for Fedora as the node-sass included one failed at runtime

--- compile.js
+++ compile.js
@@ -12,22 +12,22 @@ var sass = require('node-sass'),
 var em = new EventEmitter;
 function compileDirectory(srcDir, destDir) {
 
-	var files = require('findit').findSync(srcDir);
-	debug("files", files);
-	em.setMaxListeners(files.length);
-	for ( var int = 0; int < files.length; int++) {
-		var isLast = (int+1) == files.length;
+	var files = finder.findSync(srcDir);
+	var filesLength = files.length;
+	debug("files ["+filesLength+"]", files);
+	em.setMaxListeners(filesLength);
+	for ( var int = 0; int < filesLength; int++) {
+		var isLast = (int+1) == filesLength;
 		compile(files[int], srcDir, destDir, isLast);
 		em.once("exit", function(){
 			setTimeout(process.exit, 500);
 		});
 	}
-	
+
 }
 
 function compile(srcPath, srcDir, destDir, endProcessFlag) {
 	if(path.extname(srcPath) == '.scss') {
-
 		var srcFileName = srcPath.split(srcDir)[1];
 		var outputPath = destDir + srcFileName.replace('.scss', '.css');
 
@@ -39,27 +39,22 @@ function compile(srcPath, srcDir, destDir, endProcessFlag) {
 
 				fs.readFile(srcPath, 'utf8', function(err, str) {
 					if (err) {
-						debug("error 1", srcPath);
-						console.log(err);
+						error("ERROR readFile", "["+srcPath+"]"+err);
 						return;
 					}
 					sass.render(str, function(err, css) {
 						if (err) {
-							debug("error 2", srcPath);
-							console.log(srcPath);
-							console.log(err);
+							error("ERROR sass", "["+srcPath+"]"+err);
 							return;
 						}
 						mkdirp(dirname(outputPath), 0700, function(err) {
 							if (err) {
-								debug("error 3", outputPath);
-								console.log(err);
+								error("ERROR mkdirp", "["+srcPath+"]"+err);
 								return;
 							}
 							fs.writeFile(outputPath, css, 'utf8', function(err){
 								if (err) {
-									debug("error 4", outputPath);
-									console.log(err);
+									error("ERROR writeFile", "["+srcPath+"]"+err);
 									return;
 								} else {
 									debug('write', outputPath);
@@ -69,7 +64,7 @@ function compile(srcPath, srcDir, destDir, endProcessFlag) {
 								}
 							});
 						});
-					}, { include_paths: [ 'lib/', 'mod/' ], output_style: 'compressed' });
+					}, { include_paths: [ '../css_src' ], output_style: 'expanded' });
 				});
 			}
 		});
@@ -77,11 +72,14 @@ function compile(srcPath, srcDir, destDir, endProcessFlag) {
 }
 function debug(key, val) {
 	if(bDebug) {
-		console.error('  \033[90m%s :\033[0m \033[36m%s\033[0m', key, val);
+		console.error('  \033[37m%s :\033[0m \033[36m%s\033[0m', key, val);
 	}
 }
 function log(key, val) {
-	console.error('  \033[90m%s :\033[0m \033[36m%s\033[0m', key, val);
+	console.error('  \033[33m%s :\033[0m \033[90m%s\033[0m', key, val);
+}
+function error(key, val) {
+	console.error('  \033[31m%s :\033[0m \033[90m%s\033[0m', key, val);
 }
 process.on('message', function(data) {
 	bDebug = data.debug;
